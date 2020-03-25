9a7dd04910e6818a48a533a1b1563d1a46931d3e
yoonjoohye <67yjh00@gmail.com>
2020-03-26 00:43:39 +0900

    [add] 환경설정

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<style>
+
+</style>
+
+<svelte:head>
+	<title>Sharidea</title>
+</svelte:head>
+
+<div>
+	<div class="bg-red">hu/h</div>
+	<div class="text-2xl underline">아이디어 공유</div>
+</div>
---
+++ src/routes/write.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Write</title>
+</svelte:head>
+
+<h1>Write this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
