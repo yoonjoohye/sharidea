5fc2e0a7cf6c6fcba2520c795ef98a1d83778340
yoonjoohye <67yjh00@gmail.com>
2020-03-26 23:41:38 +0900

    [add] sapper에 tailwindcss 적용

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
-import postcss from 'rollup-plugin-postcss';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
