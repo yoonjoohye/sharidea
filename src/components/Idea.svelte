7dd14132e66f378497606d5e048de83b2261e631
yoonjoohye <67yjh00@gmail.com>
2020-03-29 01:49:47 +0900

    [add] write & idea component 추가

---
+++ src/components/Idea.svelte
@@ -0,0 +1,40 @@
+<script>
+    let list = [
+        {
+            contents: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis\n' +
+                    '        eaque, exercitationem praesentium nihil.',
+            date: '2019/03/23'
+        },
+        {
+            contents: 'asdf',
+            date: '2019/03/3'
+        },
+        {
+            contents: 'alskdjf;klasjd;fklja;skldfj',
+            date: '2019/03/13'
+        },
+        {
+            contents: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis\n' +
+                    '        eaque, exercitationem praesentium nihil.',
+            date: '2019/03/13'
+        },
+        {
+            contents: 'alskdjf;klasjd;fklja;skldfj',
+            date: '2019/03/13'
+        }
+    ]
+</script>
+
+{#each list as {contents}, idx}
+    <div class="w-full rounded overflow-hidden shadow-lg p-6">
+        <div class="mb-3">
+            <p class="text-gray-700 text-base">
+                {contents}
+            </p>
+        </div>
+        <div>
+            <span class="mr-3">하트</span>
+            <span>댓글</span>
+        </div>
+    </div>
+{/each}
---
+++ src/components/Write.svelte
@@ -0,0 +1,9 @@
+<script>
+	let text='';
+
+</script>
+
+<textarea class="resize-none w-full h-64 rounded outline-none border text-xl p-2 focus:border-blue-600"
+		  placeholder="아무런 아이디어도 좋아요"
+		  bind:value={text}
+></textarea>
