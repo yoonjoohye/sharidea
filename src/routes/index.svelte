9d44c91d4579c623a2862ba5b293da6eca70d5e6
yoonjoohye <67yjh00@gmail.com>
2020-03-28 22:25:23 +0900

    [add] 아이디어 게시물 디자인 추가

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,46 @@
+<script>
+    let list = [
+        {
+            contents: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis\n' +
+                    '        eaque, exercitationem praesentium nihil.',
+            date: '2019/03/23'
+        },
+        {
+            contents: 'asdf',
+            date: '2019/03/3'
+        },
+        {
+            contents: 'alskdjf;klasjd;fklja;skldfj',
+            date: '2019/03/13'
+        },
+        {
+            contents: 'alskdjf;klasjd;fklja;skldfj',
+            date: '2019/03/13'
+        },
+        {
+            contents: 'alskdjf;klasjd;fklja;skldfj',
+            date: '2019/03/13'
+        }
+        ]
+</script>
+
 <svelte:head>
     <title>Sharidea</title>
 </svelte:head>
 
 
-
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
-    <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p class="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+<div class="grid grid-cols-3 gap-4">
+{#each list as {contents,date}, idx}
+    <div class="max-w-sm rounded overflow-hidden shadow-lg p-6">
+        <p class="text-gray-700 text-base mb-2">
+            {contents}
         </p>
-    </div>
-    <div class="px-6 py-4">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
-    </div>
-</div>
+        <div class="mb-4">{date}</div>
 
+        <div class="flex align-items-center">
+            <div class="mr-3">하트</div>
+            <div>댓글</div>
+        </div>
+    </div>
+    {/each}
+</div>
